ab56ccd08476f10faf68e1e300ab7801c4e443e2
Blake Yeboah <blakekyeboah@gmail.com>
2021-04-02 22:20:16 +1000

    Removed Code That Wasn't Needed

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-AOS.init();
-
-const seeMyWork = document.querySelector('#seeMyWork');
-
-seeMyWork.addEventListener('click', () => {
-
-    window.location = '#projects';
-
-});
-
 window.addEventListener('scroll', () => {
 
     const scrollTop = window.pageYOffset;
@@ -27,55 +17,4 @@ window.addEventListener('scroll', () => {
         }
 
     }
-});
-
-const codeButtons = document.querySelectorAll('.code-btn');
-
-const redirect = (link) => {
-
-    window.open(link, '_blank');
-
-}
-
-codeButtons.forEach(button => button.addEventListener('click', () => {
-
-    redirect(button.getAttribute('data-github'));
-
-}));
-
-const previewButtons = document.querySelectorAll('.preview-btn');
-
-previewButtons.forEach(button => button.addEventListener('click', () => {
-
-    redirect(button.getAttribute('data-website'));
-
-}));
-
-const contactForm = document.querySelector('#contact');
-const errorAlert = document.querySelector('#contact-alert');
-
-contactForm.addEventListener('submit', e => {
-
-    const handleErr = () => {
-        e.preventDefault();
-        errorAlert.style.display = 'block';
-    }
-
-    let firstname = document.querySelector('#firstname'),
-        lastname = document.querySelector('#lastname'),
-        email = document.querySelector('#email'),
-        message = document.querySelector('#message');
-
-    if (!firstname.value || !lastname.value || !email.value || !message.value || !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
-        handleErr();
-    }
-
-});
-
-const errorClose = document.querySelector('#error-close');
-
-errorClose.addEventListener('click', () => {
-
-    errorAlert.style.display = "none";
-
 });
