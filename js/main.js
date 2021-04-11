bb698ee7315f9ca5815c90515547ab9b5b722bad
Blake Yeboah <blakekyeboah@gmail.com>
2021-04-11 18:14:44 +1000

    Added GSAP Animations

--- js/main.js
+++ js/main.js
@@ -1,20 +1,68 @@
-window.addEventListener('scroll', () => {
+// Navbar Scroll Effect
+window.addEventListener("scroll", () => {
+   const scrollTop = window.pageYOffset;
 
-    const scrollTop = window.pageYOffset;
+   const navbar = document.querySelector("#navbar");
 
-    const navbar = document.querySelector('#navbar');
+   if (window.innerWidth > 728) {
+      if (scrollTop >= 100) {
+         navbar.classList.add("contract");
+      } else {
+         navbar.classList.remove("contract");
+      }
+   }
+});
 
-    if (window.innerWidth > 728) {
+// GSAP Load In Animations
+gsap.from(".navbar", { opacity: 0, duration: 0.75, y: -100 });
+gsap.from(".anim1", {
+   opacity: 0,
+   duration: 1,
+   stagger: 0.3,
+   y: -30,
+   delay: 0.75,
+});
 
-        if (scrollTop >= 100) {
+// Navigation Links Scroll Animation
+document.querySelectorAll(".nav-link").forEach((link) => {
+   link.addEventListener("click", () => {
+      gsap.to(window, {
+         duration: 0.75,
+         scrollTo: { y: link.getAttribute("data-section"), offsetY: 50 },
+         ease: "power1",
+      });
+   });
+});
 
-            navbar.classList.add('contract');
-
-        } else {
-
-            navbar.classList.remove('contract');
-
-        }
-
-    }
+// Scroll To Projects Event Listener
+document.querySelector(".scroll-down-btn").addEventListener("click", () => {
+   gsap.to(window, {
+      duration: 0.75,
+      scrollTo: { y: "#projects", offsetY: 50 },
+      ease: "power1",
+   });
+   gsap.from(".section-title", {
+      duration: 0.5,
+      opacity: 0,
+      y: -50,
+      delay: 0.35,
+   });
+   gsap.from(".section-sub-text", {
+      duration: 0.5,
+      opacity: 0,
+      y: -50,
+      delay: 0.5,
+   });
+   gsap.from(".slide-right", {
+      duration: 1,
+      opacity: 0,
+      x: -150,
+      delay: 0.5,
+   });
+   gsap.from(".slide-left", {
+      duration: 1,
+      opacity: 0,
+      x: 150,
+      delay: 0.5,
+   });
 });
